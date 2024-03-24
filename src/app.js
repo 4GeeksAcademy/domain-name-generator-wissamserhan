2ef6de6878efb94fddada7ad896a9bd55b05fd48
wissamserhan <116583162+wissamserhan@users.noreply.github.com>
2024-03-24 22:46:14 +0000

    proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  for (let i = 0; i < 8; i++) {
+    let randomPronoun = getRandomElement(pronoun);
+    let randomAdj = getRandomElement(adj);
+    let randomNoun = getRandomElement(noun);
+    console.log(randomPronoun + randomAdj + randomNoun + ".com");
+  }
 };
